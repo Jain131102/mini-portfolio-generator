e5c06791048ce4b8f509d0e91edcd8617335d6f2
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2025-09-06 01:46:59 +0530

    Add allowedHosts to Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['mini-portfolio-generator.onrender.com'],
   },
   plugins: [
     react(),
